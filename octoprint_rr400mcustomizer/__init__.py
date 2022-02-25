4cc2cf9c8e51689d37dfcd70c699dbcdce3eedb6
Rebel3D-CZ <69604290+Rebel3D-CZ@users.noreply.github.com>
2022-02-25 21:57:22 +0100

    Update __init__.py

--- octoprint_rr400mcustomizer/__init__.py
+++ octoprint_rr400mcustomizer/__init__.py
@@ -139,7 +139,7 @@ class RR400MCustomizerPlugin(
             try:
                 currentData = self._printer.get_current_data()
                 progressPerc = int(currentData["progress"]["completion"])
-                self._printer.commands("M118 A1 P0 action:notification Data Left {}/100".format(progressPerc))
+                self._printer.commands("M118 A1 P2 action:notification Data Left {}/100".format(progressPerc))
             except Exception as e:
                 self._logger.info("Caught an exception {0}\nTraceback:{1}".format(e, traceback.format_exc()))
 
@@ -169,7 +169,7 @@ class RR400MCustomizerPlugin(
                 if len(sys_ip) > 7 and self.wifimode != 'wlan':
                     self._logger.debug("RR400mMCstomizer: WiFi switched to WLAN mode")
                     self.wifimode = 'wlan'
-                    self._printer.commands("M118 A1 P0 action:notification IP %s" % (sys_ip))
+                    self._printer.commands("M118 A1 P2 action:notification IP %s" % (sys_ip))
 #                    self._logger.info("%s: GCODE WLAN ip=%s" % (__plugin_name__, sys_ip))
                 return
 
@@ -180,7 +180,7 @@ class RR400MCustomizerPlugin(
                 if self.wifimode != 'ap':
                     self._logger.debug("RR400mMCstomizer: WiFi switched to AP mode")
                     self.wifimode = 'ap'
-                    self._printer.commands("M118 A1 P0 action:notification SSID %s IP %s" % (socket.gethostname(), sys_ip))
+                    self._printer.commands("M118 A1 P2 action:notification SSID %s IP %s" % (socket.gethostname(), sys_ip))
 #                    self._logger.info("%s: GCODE AP ip=%s" % (__plugin_name__, sys_ip))
                 return
         except Exception as exc:
