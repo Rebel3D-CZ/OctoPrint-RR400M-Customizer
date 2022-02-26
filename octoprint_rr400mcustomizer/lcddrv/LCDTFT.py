96342f1c40e14c0e035c02058314c0770b1d9036
Rebel3D-CZ <69604290+Rebel3D-CZ@users.noreply.github.com>
2022-02-26 21:06:45 +0100

    Update LCDTFT.py

--- octoprint_rr400mcustomizer/lcddrv/LCDTFT.py
+++ octoprint_rr400mcustomizer/lcddrv/LCDTFT.py
@@ -16,7 +16,7 @@ class LCDTFT:
     return
 
   def updateProgress(self, printer, progressPerc):
-    printer.commands("M118 A1 P0 action:notification Data Left {}/100".format(progressPerc))
+    printer.commands("M118 A1 P2 action:notification Data Left {}/100".format(progressPerc))
     return
 
   def notify(self, printer, message):
